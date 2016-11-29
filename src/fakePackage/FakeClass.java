b3725fd6be3ece50ef871f945494172b142a3ae5
Wilbert <wilbertveit@rocketmail.com>
2016-11-28 20:09:04 -0800

    Added really cooooool code dude

--- src/fakePackage/FakeClass.java
+++ src/fakePackage/FakeClass.java
@@ -5,6 +5,9 @@ public class FakeClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		
+		for(int i = 0; i < 50; i++)
+			System.out.println("really awesome code duuuude!");
 	}
 
 }
