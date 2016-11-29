d14b08c9c47976f62a471acc3c52562a991e17fc
Wilbert <wilbertveit@rocketmail.com>
2016-11-28 20:22:42 -0800

    waka waka waka

--- src/fakePackage/FakeClass.java
+++ src/fakePackage/FakeClass.java
@@ -8,10 +8,16 @@ public class FakeClass {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
 		
-		for(int i = 0; i < 50; i++)
-			System.out.println("really awesome code duuuude!");
+		for(int i = 99; i >= 0; i--){
+			System.out.println("\n===================================================");
+			System.out.println(i + " bottles of beer on the wall,");
+			System.out.println(i + " bottles of beer.");
+			System.out.println("If one of those bottles should happen to fall,");
+			System.out.println(i - 1 + " bottles of beer on the wall.");
+			System.out.println("===================================================");
+		}
 		
-		JOptionPane.showMessageDialog(null, "My Goodness, this project is better than nails in my eyes!");
+		JOptionPane.showMessageDialog(null, "This project is better than nails in my eyes!");
 	}
 
 }
